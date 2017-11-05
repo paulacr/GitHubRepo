e0558fe1200d75a247343f6c1b4cb85c0cd8d62d
Paula <paula.mecatronica@gmail.com>
2017-11-05 01:25:35 -0200

    deleted unused files

--- GitHubRepo/app/src/main/java/net/paulacr/githubrepo/utils/OnListItemClick.java
+++
@@ -1,6 +0,0 @@
-package net.paulacr.githubrepo.utils;
-
-public interface OnListItemClick {
-
-    void onListItemClicked(int position);
-}
--- GitHubRepo/app/src/main/java/net/paulacr/githubrepo/utils/OnReceiverNetworkStatus.java
+++
@@ -1,7 +0,0 @@
-package net.paulacr.githubrepo.utils;
-
-public interface OnReceiverNetworkStatus {
-
-    void onReceive(boolean isConnected);
-
-}
